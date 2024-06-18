29ed5c92ae6706f40116f08fdfa58f94589007af
zhangjin <23231269@qq.com>
2024-06-18 23:46:26 +0800

    使用全新的依赖环境。

--- src/main/java/uw/code/center/controller/ops/log/SysDataHistoryController.java
+++ src/main/java/uw/code/center/controller/ops/log/SysDataHistoryController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import uw.app.common.dto.SysCritLogQueryParam;
-import uw.app.common.entity.SysCritLog;
+import uw.app.common.dto.SysDataHistoryQueryParam;
+import uw.app.common.entity.SysDataHistory;
 import uw.auth.service.AuthServiceHelper;
 import uw.auth.service.annotation.MscPermDeclare;
 import uw.auth.service.constant.ActionLog;
@@ -20,43 +20,43 @@ import uw.dao.TransactionException;
 
 
 /**
- * 关键日志管理。
+ * 数据历史管理。
  */
 @RestController
 @MscPermDeclare(type = UserType.OPS)
-@Tag(name = "关键日志管理", description = "关键日志管理")
-@RequestMapping("/ops/log/crit")
-public class CritLogController {
+@Tag(name = "数据历史管理", description = "数据历史管理")
+@RequestMapping("/ops/log/history")
+public class SysDataHistoryController {
 
     DaoFactory dao = DaoFactory.getInstance();
 
     /**
-     * 列表OPS关键日志。
+     * 列表数据历史。
      *
      * @param queryParam
      * @return
      * @throws TransactionException
      */
     @MscPermDeclare(type = UserType.OPS, auth = AuthType.PERM, log = ActionLog.REQUEST)
-    @Operation(summary = "列表关键日志", description = "列表关键日志")
+    @Operation(summary = "列表数据历史", description = "列表数据历史")
     @GetMapping("/list")
-    public DataList<SysCritLog> list(SysCritLogQueryParam queryParam) throws TransactionException {
-        AuthServiceHelper.logInfo( SysCritLog.class, 0, "列表关键日志" );
-        return dao.list( SysCritLog.class, queryParam );
+    public DataList<SysDataHistory> list(SysDataHistoryQueryParam queryParam) throws TransactionException {
+        AuthServiceHelper.logInfo(SysDataHistory.class, 0, "列表saas系统修改数据历史");
+        return dao.list(SysDataHistory.class, queryParam);
     }
 
     /**
-     * 加载关键日志。
+     * 加载数据历史。
      *
      * @param id
      * @throws TransactionException
      */
     @MscPermDeclare(type = UserType.OPS, auth = AuthType.PERM, log = ActionLog.REQUEST)
-    @Operation(summary = "加载关键日志", description = "加载关键日志")
+    @Operation(summary = "加载数据历史", description = "加载数据历史")
     @GetMapping("/load")
-    public SysCritLog load(@Parameter(description = "主键ID", required = true, example = "1") @RequestParam long id) throws TransactionException {
-        AuthServiceHelper.logInfo( SysCritLog.class, id, "加载关键日志" );
-        return dao.load( SysCritLog.class, id );
+    public SysDataHistory load(@Parameter(description = "主键ID", required = true, example = "1") @RequestParam long id) throws TransactionException {
+        AuthServiceHelper.logInfo(SysDataHistory.class, id, "加载saas系统修改数据历史");
+        return dao.load(SysDataHistory.class, id);
     }
 
 }
