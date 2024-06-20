041828c4eda6539dc1deae7f0eb9b788f0751d66
zhangjin <23231269@qq.com>
2024-06-20 15:40:06 +0800

    代码规范化。

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,3 +1,34 @@
+-- code.code_template_group definition
+
+CREATE TABLE `code_template_group` (
+                                       `id` bigint NOT NULL COMMENT 'id',
+                                       `group_type` int DEFAULT NULL COMMENT '数据源类型',
+                                       `group_name` varchar(100) DEFAULT NULL COMMENT '模板组名',
+                                       `group_desc` varchar(100) DEFAULT NULL COMMENT '模板组描述',
+                                       `create_date` datetime(3) DEFAULT NULL COMMENT '创建日期',
+                                       `modify_date` datetime(3) DEFAULT NULL COMMENT '修改日期',
+                                       `state` int NOT NULL COMMENT '状态。1正常-1标记删除',
+                                       PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='代码模版组';
+
+
+-- code.code_template_info definition
+
+CREATE TABLE `code_template_info` (
+                                      `id` bigint NOT NULL COMMENT 'id',
+                                      `group_id` bigint NOT NULL COMMENT '模板分组id',
+                                      `template_type` int DEFAULT NULL COMMENT '数据类型',
+                                      `template_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '模板名称',
+                                      `template_desc` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '模板描述',
+                                      `template_filename` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '输出文件名模板',
+                                      `template_body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '模板内容',
+                                      `create_date` datetime(3) DEFAULT NULL COMMENT '创建日期',
+                                      `modify_date` datetime(3) DEFAULT NULL COMMENT '修改日期',
+                                      `state` int NOT NULL COMMENT '状态。1正常-1标记删除',
+                                      PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='代码模版';
+
+
 -- code.sys_crit_log definition
 
 CREATE TABLE `sys_crit_log` (
@@ -12,18 +43,19 @@ CREATE TABLE `sys_crit_log` (
                                 `real_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '真实名称',
                                 `ref_type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '操作对象类型',
                                 `ref_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '操作对象id',
-                                `uri` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '请求uri',
-                                `info` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '方法操作描述',
-                                `log` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '日志内容',
-                                `request_body` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '请求参数',
-                                `response_body` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '响应日志',
+                                `api_uri` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '请求uri',
+                                `api_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'API名称',
+                                `op_state` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '操作状态',
+                                `op_log` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '日志内容',
+                                `request_body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '请求参数',
+                                `response_body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '响应日志',
                                 `response_millis` bigint DEFAULT NULL COMMENT '请求毫秒数',
                                 `exception` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '异常信息',
                                 `status_code` int DEFAULT NULL COMMENT '响应状态码',
                                 `app_info` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '应用信息',
                                 `app_host` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '应用主机',
-                                `user_ip` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '操作人ip',
-                                `request_date` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
+                                `user_ip` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户ip',
+                                `request_date` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '请求时间',
                                 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='系统关键日志';
 
@@ -44,42 +76,12 @@ CREATE TABLE `sys_data_history` (
                                     `entity_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '实体ID',
                                     `entity_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '实体名',
                                     `entity_data` json DEFAULT NULL COMMENT '实体数据',
-                                    `entity_update_info` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '实体修改信息',
+                                    `entity_update_info` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '实体修改信息',
                                     `remark` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '备注信息',
                                     `user_ip` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户IP',
                                     `create_date` datetime(3) DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建日期',
                                     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='系统数据历史';
-
--- code.code_template definition
-
-CREATE TABLE `code_template` (
-                                 `id` bigint NOT NULL COMMENT 'id',
-                                 `group_id` bigint NOT NULL COMMENT '模板分组id',
-                                 `template_type` int DEFAULT NULL COMMENT '数据类型',
-                                 `template_name` varchar(100) DEFAULT NULL COMMENT '模板名称',
-                                 `template_desc` varchar(100) DEFAULT NULL COMMENT '模板描述',
-                                 `template_filename` varchar(100) DEFAULT NULL COMMENT '输出文件名模板',
-                                 `template_body` longtext COMMENT '模板内容',
-                                 `create_date` datetime(3) DEFAULT NULL COMMENT '创建日期',
-                                 `modify_date` datetime(3) DEFAULT NULL COMMENT '修改日期',
-                                 `state` int NOT NULL COMMENT '状态。1正常-1标记删除',
-                                 PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='代码模版';
-
-
--- code.code_template_group definition
-
-CREATE TABLE `code_template_group` (
-                                       `id` bigint NOT NULL COMMENT 'id',
-                                       `group_type` int DEFAULT NULL COMMENT '数据源类型',
-                                       `group_name` varchar(100) DEFAULT NULL COMMENT '模板组名',
-                                       `group_desc` varchar(100) DEFAULT NULL COMMENT '模板组描述',
-                                       `create_date` datetime(3) DEFAULT NULL COMMENT '创建日期',
-                                       `modify_date` datetime(3) DEFAULT NULL COMMENT '修改日期',
-                                       `state` int NOT NULL COMMENT '状态。1正常-1标记删除',
-                                       PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='代码模版组';
 
 
 -- code.sys_seq definition
