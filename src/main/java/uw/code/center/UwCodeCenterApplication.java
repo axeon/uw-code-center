9c0c1e947fac94581dfb4a69d24d3a987cc64db7
zhangjin <23231269@qq.com>
2025-04-11 00:26:45 +0800

    build(uw): 升级基础版本并添加服务发现支持
    - 将 uw-base 版本从 2025.0330.1003升级到 2025.0330.1005
    - 在 UwCodeCenterApplication 中添加 @EnableDiscoveryClient 注解以支持服务发现
    - 优化 GlobalMscAclFilter 类中的代码格式和异常处理

--- src/main/java/uw/code/center/UwCodeCenterApplication.java
+++ src/main/java/uw/code/center/UwCodeCenterApplication.java
@@ -2,9 +2,11 @@ package uw.code.center;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.AnnotationBeanNameGenerator;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UwCodeCenterApplication {
 
     public static void main(String[] args) {
