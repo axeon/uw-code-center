9f818b8258dc449ecd6ccf83eb2392485fbf7f05
zhangjin <23231269@qq.com>
2025-06-24 21:50:23 +0800

    feat(test): 添加测试接口
    - 新增 DaoController 类，提供测试用的接口
    - 包含两个接口：list 和 load
    - 使用 @Profile 注解限制在 test 和 dev 环境下启用

---
+++ src/main/java/uw/code/center/controller/open/DaoController.java
@@ -0,0 +1,36 @@
+package uw.code.center.controller.open;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import uw.code.center.entity.CodeTemplateGroup;
+import uw.code.center.entity.CodeTemplateInfo;
+import uw.common.dto.ResponseData;
+import uw.dao.DaoManager;
+import uw.dao.DataList;
+
+/**
+ * 测试接口
+ */
+@RestController
+@RequestMapping("/open/dao")
+@Tag(name = "测试接口")
+@Profile({"test", "dev"})
+public class DaoController {
+
+    DaoManager dao = DaoManager.getInstance();
+
+    @GetMapping("/list")
+    public ResponseData<DataList<CodeTemplateGroup>> list() {
+        return dao.list(CodeTemplateGroup.class, "");
+    }
+
+    @GetMapping("/load")
+    public ResponseData<CodeTemplateInfo> load(@RequestParam long id) {
+        return dao.load(CodeTemplateInfo.class, id);
+    }
+
+}
