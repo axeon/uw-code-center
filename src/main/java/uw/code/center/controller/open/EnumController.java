be201a0ae88f6f9cec23dc91624d81d8a1cc83ae
zhangjin <23231269@qq.com>
2025-12-10 17:47:07 +0800

    feat(enum): 优化枚举获取接口返回结构
    - 将 getAllEnumMap 接口返回类型从 Map<String, Object> 改为 Map<String, Map<String, Enum<?>>>
    - 升级多个模块的父依赖版本从 2025.1101.0002 到 2025.1101.0003

--- src/main/java/uw/code/center/controller/open/EnumController.java
+++ src/main/java/uw/code/center/controller/open/EnumController.java
@@ -5,14 +5,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import uw.common.util.DateUtils;
 import uw.common.util.EnumUtils;
 
-import java.util.Date;
 import java.util.Map;
-import java.util.TimeZone;
 
 /**
  * 枚举类管理
@@ -27,7 +23,7 @@ public class EnumController {
 
     @GetMapping("/getAllEnumMap")
     @Operation(summary = "获取所有枚举", description = "获取所有枚举")
-    public Map<String, Object> getAllEnumMap() {
+    public Map<String, Map<String, Enum<?>>> getAllEnumMap() {
         return EnumUtils.getEnumMap(BASE_PACKAGE);
     }
 
